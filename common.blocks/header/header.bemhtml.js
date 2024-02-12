b597ab33dbf109e489654ae7bf8e33bc6fe98825
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2024-02-12 20:24:12 +0300

    improvement: use icons for tel and main in header

--- common.blocks/header/header.bemhtml.js
+++ common.blocks/header/header.bemhtml.js
@@ -9,15 +9,34 @@ block('header')(
         block: 'navigation',
       },
       {
-        block: 'main-contacts',
-      },
-      {
-          elem: 'bar',
-          content: [
-              {
-                  block: 'theme-switcher',
-              },
-          ]
+        elem: 'bar',
+        content: [
+          {
+            block: 'button',
+            mix: { block: 'header', elem: 'icon-wrapper' },
+            mods: { type: 'link' },
+            url: 'tel:+79268555282',
+            icon: {
+              block: 'icon',
+              mix: { block: 'header', elem: 'icon' },
+              url: '/static/images/icons/phone.svg',
+            }
+          },
+          {
+            block: 'button',
+            mix: { block: 'header', elem: 'icon-wrapper' },
+            mods: { type: 'link' },
+            url: 'mailto:kreoshine@bk.ru',
+            icon: {
+              block: 'icon',
+              mix: { block: 'header', elem: 'icon' },
+              url: '/static/images/icons/mail.svg',
+            }
+          },
+          {
+              block: 'theme-switcher',
+          },
+        ]
       },
     ];
   }),
--- common.blocks/header/header.deps.js
+++ common.blocks/header/header.deps.js
@@ -1,6 +1,11 @@
 ({
     block: 'header',
     shouldDeps: [
+        // lib
+        { block: 'link' },
+        { block: 'icon' },
+        { block: 'button', mods: { type: 'link'} },
+
         // common
         { block: 'main-contacts' },
         { block: 'navigation' },
