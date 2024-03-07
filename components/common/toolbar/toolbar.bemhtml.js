9014000b4e3a59457e1eabe89c437cab1c5c0319
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2024-03-07 19:14:42 +0300

    improvement: use dropdown for toolbar

--- components/common/toolbar/toolbar.bemhtml.js
+++ components/common/toolbar/toolbar.bemhtml.js
@@ -1,31 +1,65 @@
 block('toolbar')(
 	content()(function() {
-		return [
-			{
+		return {
+			block: 'dropdown',
+			mods: {
+					switcher: 'button',
+					theme: 'islands',
+			},
+			switcher: {
 				block: 'button',
-				mix: { block: 'toolbar', elem: 'icon-wrapper' },
-				mods: { type: 'link' },
-				url: 'tel:+79268555282',
+				mix: { block: 'toolbar', elem: 'button' },
 				icon: {
 					block: 'icon',
 					mix: { block: 'toolbar', elem: 'icon' },
-					url: '/static/images/icons/phone.svg',
+					url: '/static/images/icons/toolbar.svg',
 				}
 			},
-			{
-				block: 'button',
-				mix: { block: 'toolbar', elem: 'icon-wrapper' },
-				mods: { type: 'link' },
-				url: 'mailto:kreoshine@bk.ru',
-				icon: {
-					block: 'icon',
-					mix: { block: 'toolbar', elem: 'icon' },
-					url: '/static/images/icons/mail.svg',
-				}
-			},
-			{
-				block: 'theme-switcher',
-			},
-		];
+			popup: {
+				block: 'menu',
+				mix: { block: 'toolbar', elem: 'menu' },
+				content: [
+					{
+						block: 'button',
+						mix: { block: 'toolbar', elem: 'icon-wrapper' },
+						mods: { type: 'link' },
+						url: 'tel:+79268555282',
+						icon: {
+							block: 'icon',
+							mix: { block: 'toolbar', elem: 'icon' },
+							url: '/static/images/icons/phone.svg',
+						}
+					},
+					{
+						block: 'button',
+						mix: { block: 'toolbar', elem: 'icon-wrapper' },
+						mods: { type: 'link' },
+						url: 'mailto:kreoshine@bk.ru',
+						icon: {
+							block: 'icon',
+							mix: { block: 'toolbar', elem: 'icon' },
+							url: '/static/images/icons/mail.svg',
+						}
+					},
+					{
+						block: 'theme-switcher',
+					},
+				]
+			}
+		}
+			// {
+			// 	block: 'link',
+			// 	mods: {
+			// 			pseudo: true,
+			// 			theme: 'islands'
+			// 	},
+			// 	content: 'open islands popup with content interaction'
+			// },
+			// {
+			// 	block: 'popup',
+			// 	mods : { theme: 'islands', target : 'anchor', autoclosable: true },
+			// 	// directions : ['bottom-left'],
+
+		// ]
 	})
 );
--- components/common/toolbar/toolbar.deps.js
+++ components/common/toolbar/toolbar.deps.js
@@ -2,12 +2,34 @@
     block: 'header',
     shouldDeps: [
         // lib
-        { block: 'link' },
+        {
+            block: 'link',
+            mods: {
+                pseudo: true,
+                theme: 'island',
+            }
+        },
+        {
+            block: 'dropdown',
+            mods: {
+                switcher: 'button',
+                theme: 'islands',
+                // size: 'm'
+            }
+        },
+        {
+            block: 'popup',
+            mods: {
+                theme: 'islands',
+                target: 'anchor',
+                autoclosable: true
+            }
+        },
+        { block: 'menu' },
         { block: 'icon' },
         { block: 'button', mods: { type: 'link'} },
 
         // design
         { block: 'theme-switcher' },
-        { block: 'header', mod: 'theme' },
     ],
 })
